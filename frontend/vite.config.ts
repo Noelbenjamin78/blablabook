5122399bc34d15f43e59d2664a6012174c1e6232
pierrealf <pierre.alexis.56@gmail.com>
2025-04-07 15:33:13 +0200

    feat(front): installed tailwind shadcn prettier eslint

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import tailwindcss from '@tailwindcss/vite';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: true,
-    port: 3000,
-  },
+    plugins: [react(), tailwindcss()],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        },
+    },
+    server: {
+        host: true,
+        port: 3000,
+    },
 });
