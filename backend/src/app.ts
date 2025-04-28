a39f0f3d41ae33e17b32ce552075f7191a478fe3
YannickSendrey <yannicksendrey@gmail.com>
2025-04-28 10:23:16 +0200

    fix: allow CORS backend address for both dev & prod env
    Signed-off-by: YannickSendrey <yannicksendrey@gmail.com>

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -11,9 +11,23 @@ dotenv.config();
 
 const app = express();
 
+const allowedOrigins = [
+  'http://localhost:3000',
+  'http://yannicksendrey-server.eddi.cloud:3000',
+  'https://blablabook.live'
+];
+
+// Middleware to handle CORS
 app.use(cors({
-    origin: 'http://localhost:3000'
-  }));
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error(`CORS blocked: origin ${origin} not allowed.`));
+    }
+  },
+  credentials: true
+}));
 app.use(express.json());
 
 app.use('/api/books', booksRoutes);
