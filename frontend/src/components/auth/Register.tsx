8379034380f804de10b390d5a5aaaf72be5a5293
Noelbenjamin78 <noel.benjamin78@gmail.com>
2025-04-28 11:27:53 +0200

    redirection login et register + fix couleur du h1 sur login + refresh

--- frontend/src/components/auth/Register.tsx
+++ frontend/src/components/auth/Register.tsx
@@ -67,7 +67,7 @@ const Register: React.FC<{ onRegister: (name: string, email: string, password: s
             }
 
             alert('Inscription réussie !');
-            window.location.href = '/accueil';
+            window.location.href = '/login';
         } catch (error) {
             console.error("Erreur lors de l'inscription :", error);
             alert('Une erreur est survenue.');
@@ -173,11 +173,6 @@ const Register: React.FC<{ onRegister: (name: string, email: string, password: s
                                         <FormMessage />
                                     </FormItem>
                                 )}
-                            />
-                            <Button
-                                type="submit"
-                                variant="defaultNoHover"
-                                className="w-full bg-app-bg text-black border border-black"
                             />
                             <Button
                                 type="submit"
