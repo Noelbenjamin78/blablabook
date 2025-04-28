13c52881ed43807909910f14f8ff685a9352d6a3
YannickSendrey <yannicksendrey@gmail.com>
2025-04-28 10:19:17 +0200

    fix: allow VM address in vite.config.ts
    Signed-off-by: YannickSendrey <yannicksendrey@gmail.com>

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -16,5 +16,8 @@ export default defineConfig({
     server: {
         host: true,
         port: 3000,
+        allowedHosts: [
+            "yannicksendrey-server.eddi.cloud"
+          ]
     },
 });
