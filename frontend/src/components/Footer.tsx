5ea9e09a57ca53ad4f348f2dc46a18742f7e6f9b
pierrealf <pierre.alexis.56@gmail.com>
2025-04-30 15:55:08 +0200

    fix: reduced cards size & changed href with link & booktoread counter works & framer motion mobile menu burger & removed sensitive console logs

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,13 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Footer: React.FC = () => {
   return (
     <footer className="bg-secondary-blue fixed bottom-0 w-full py-4 text-white">
       <div className="text-center">
-        <a href="/rgpd" className="mr-5">
+        <Link to="/rgpd" className="mr-5">
           Politique de confidentialité
-        </a>
-        <a href="apropos">À propos</a>
+        </Link>
+        <Link to="apropos">À propos</Link>
         <p className="text-sm">© 2025 BlaBlaBook. Tous droits réservés.</p>
       </div>
     </footer>
